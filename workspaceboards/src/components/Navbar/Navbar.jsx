bfb8e7743b7d47a8302a9b40e0f347c62d0dbe07
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-24 18:14:30 +0500

    fix: navbar styles

--- workspaceboards/src/components/Navbar/Navbar.jsx
+++ workspaceboards/src/components/Navbar/Navbar.jsx
@@ -15,6 +15,10 @@ import AccountCircle from "@mui/icons-material/AccountCircle";
 import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import MoreIcon from "@mui/icons-material/MoreVert";
+import Logo from "../../public/Logo/gettyimages-1447466279-640x640-removebg-preview.png";
+import NotificationIcon from "../../public/Icons/notifications.png";
+import ProfileIcon from "../../public/Icons/profile.png";
+import "./navbar.css";
 
 // const Search = styled("div")(({ theme }) => ({
 //   position: "relative",
@@ -85,7 +89,7 @@ const Navbar = () => {
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: "top",
+        vertical: "bottom",
         horizontal: "right",
       }}
       id={menuId}
@@ -96,11 +100,87 @@ const Navbar = () => {
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
+      sx={{
+        marginTop: "15px",
+        backgroundColor: "#FF",
+        opacity: "1",
+      }}
+      MenuListProps={{
+        sx: {
+          backgroundColor: "#FF",
+        },
+      }}
     >
-      <MenuItem onClick={handleMenuClose}>Dashboard</MenuItem>
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>Edit Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>Sign Out</MenuItem>
+      <MenuItem
+        onClick={handleMenuClose}
+        sx={{
+          width: "100%",
+          borderLeft: "50px solid transparent",
+          borderRight: "50px solid transparent",
+          textAlign: "center",
+          justifyContent: "center",
+          outline: "5px solid #4d2e19",
+          backgroundColor: "#825638",
+          "&:hover": {
+            backgroundColor: "#5e3d27", // Change the background color on hover
+          },
+        }}
+      >
+        Dashboard
+      </MenuItem>
+      <MenuItem
+        onClick={handleMenuClose}
+        sx={{
+          width: "100%",
+          borderLeft: "50px solid transparent",
+          borderRight: "50px solid transparent",
+          justifyContent: "center",
+          outline: "5px solid #4d2e19",
+          marginTop: "10%",
+          backgroundColor: "#825638",
+          "&:hover": {
+            backgroundColor: "#5e3d27", // Change the background color on hover
+          },
+        }}
+      >
+        Profile
+      </MenuItem>
+      <MenuItem
+        onClick={handleMenuClose}
+        sx={{
+          width: "100%",
+          borderLeft: "50px solid transparent",
+          borderRight: "50px solid transparent",
+          textAlign: "center",
+          justifyContent: "center",
+          outline: "5px solid #4d2e19",
+          marginTop: "10%",
+          backgroundColor: "#825638",
+          "&:hover": {
+            backgroundColor: "#5e3d27", // Change the background color on hover
+          },
+        }}
+      >
+        Edit Profile
+      </MenuItem>
+      <MenuItem
+        onClick={handleMenuClose}
+        sx={{
+          width: "100%",
+          borderLeft: "50px solid transparent",
+          borderRight: "50px solid transparent",
+          textAlign: "center",
+          justifyContent: "center",
+          outline: "5px solid #4d2e19",
+          marginTop: "10%",
+          backgroundColor: "#825638",
+          "&:hover": {
+            backgroundColor: "#5e3d27", // Change the background color on hover
+          },
+        }}
+      >
+        Sign Out
+      </MenuItem>
     </Menu>
   );
 
@@ -121,14 +201,14 @@ const Navbar = () => {
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
-      <MenuItem>
+      {/* <MenuItem>
         <IconButton size="large" aria-label="show 4 new mails" color="inherit">
           <Badge badgeContent={4} color="error">
             <MailIcon />
           </Badge>
         </IconButton>
         <p>Messages</p>
-      </MenuItem>
+      </MenuItem> */}
       <MenuItem>
         <IconButton
           size="large"
@@ -179,14 +259,15 @@ const Navbar = () => {
           >
             <MenuIcon />
           </IconButton> */}
-          <Typography
+          {/* <Typography
             variant="h6"
             noWrap
             component="div"
             sx={{ display: { xs: "none", sm: "block" } }}
           >
             MUI
-          </Typography>
+          </Typography> */}
+          <img src={Logo} alt="logo" className="navbar-logo"></img>
           {/* <Search>
             <SearchIconWrapper>
               <SearchIcon />
@@ -211,9 +292,18 @@ const Navbar = () => {
               size="large"
               aria-label="show 17 new notifications"
               color="inherit"
+              sx={{
+                "&:hover": {
+                  backgroundColor: "transparent", // Change the background color on hover
+                },
+              }}
             >
               <Badge badgeContent={18} color="error">
-                <NotificationsIcon />
+                <img
+                  src={NotificationIcon}
+                  alt="Notifications"
+                  className="navbar-icons"
+                />
               </Badge>
             </IconButton>
             <IconButton
@@ -224,8 +314,17 @@ const Navbar = () => {
               aria-haspopup="true"
               onClick={handleProfileMenuOpen}
               color="inherit"
+              sx={{
+                "&:hover": {
+                  backgroundColor: "transparent", // Change the background color on hover
+                },
+              }}
             >
-              <AccountCircle />
+              <img
+                src={ProfileIcon}
+                alt="Profile"
+                className="navbar-icons profile"
+              />
             </IconButton>
           </Box>
           <Box sx={{ display: { xs: "flex", md: "none" } }}>
