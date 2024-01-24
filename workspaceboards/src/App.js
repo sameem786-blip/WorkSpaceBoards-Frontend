3120df080d01ed8cad88fc3125c7bd9635990c87
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-24 14:13:01 +0500

    update: app.js prep for context

--- workspaceboards/src/App.js
+++ workspaceboards/src/App.js
@@ -1,11 +1,58 @@
 import "./App.css";
 import { useContext } from "react";
 import { AuthContext } from "./context/authContext";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
   const { currentUser } = useContext(AuthContext);
+  const queryClient = new QueryClient();
 
-  return <div className="App"></div>;
+  // const Layout = () => {
+  //   return (
+  //     <QueryClientProvider client={queryClient}>
+  //     <div className={`theme-${darkMode ? "dark" : "light"}`}>
+  //       <Navbar />
+  //       <div style={{display:"flex"}}>
+  //         <Leftbar />
+  //         <div style={{flex:6}}>
+  //           <Outlet />
+  //         </div>
+  //         <Rightbar />
+  //       </div>
+  //       </div>
+  //       </QueryClientProvider>
+  //   )
+  // }
+
+  // const ProtectedRoute = ({ children }) => {
+  //   if (!currentUser) {
+  //     return <Navigate to="/login" />
+  //   }
+
+  //   return children;
+  // }
+
+  return (
+    <div className="App">
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter> */}
+    </div>
+  );
 }
 
 export default App;
