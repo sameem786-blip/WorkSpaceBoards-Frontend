423c8d89e770c6374b053fd114b28d55d87c7092
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-25 16:20:45 +0500

    update: testing navbar

--- workspaceboards/src/App.js
+++ workspaceboards/src/App.js
@@ -45,8 +45,7 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      <Auth />
+      <Navbar />
       {/* <BrowserRouter>
         <Routes>
           <Route path="/register" element={<Register />} />
