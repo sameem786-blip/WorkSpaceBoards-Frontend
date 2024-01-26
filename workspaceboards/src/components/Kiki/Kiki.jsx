6803c5aa5fa81b1c05c719655ad4631190b335c5
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-26 17:04:15 +0500

    update: flipping kiki

--- workspaceboards/src/components/Kiki/Kiki.jsx
+++ workspaceboards/src/components/Kiki/Kiki.jsx
@@ -4,8 +4,8 @@ import "./kiki.css";
 const Kiki = ({ flipped }) => {
   return (
     <div className={flipped ? "kiki-container-flipped" : "kiki-container"}>
-      <div class="moon"></div>
-      <div class="kiki bounce"></div>
+      <div className={flipped ? "moon-reversed" : "moon"}></div>
+      <div className={flipped ? "kiki-reversed bounce" : "kiki bounce"}></div>
     </div>
   );
 };
