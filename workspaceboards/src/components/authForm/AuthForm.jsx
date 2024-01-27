9d33d2a3d84d696a0409ab802442124a3a9d977d
sameem786-blip <70144568+sameem786-blip@users.noreply.github.com>
2024-01-28 04:33:47 +0500

    update: btn added to auth

--- workspaceboards/src/components/authForm/AuthForm.jsx
+++ workspaceboards/src/components/authForm/AuthForm.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import "./authForm.css";
 import AuthInput from "../AuthInput/AuthInput"
+import ForwardIcon from '@mui/icons-material/Forward';
 
 const AuthForm = ({login}) => {
   return (
       <div className='authform-container'>
           <AuthInput content="email"/>
           <AuthInput />
+          <div className="btn-container">
+              <div className="btn-row">
+                  <div className="green-arrow-container"><ForwardIcon className='arrow-icon' /></div>
+                    <button className='btn'>Submit</button>
+              </div>
+          </div>
     </div>
   )
 }
