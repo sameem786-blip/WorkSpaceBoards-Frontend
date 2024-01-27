7a6cf4cc39acb2dc26994b1d73846ccc45d8b688
sameem786-blip <70144568+sameem786-blip@users.noreply.github.com>
2024-01-27 22:07:06 +0500

    update:loader removed from app.js

--- workspaceboards/src/App.js
+++ workspaceboards/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState,useContext } from "react";
+import { useContext } from "react";
 import { AuthContext } from "./context/authContext";
 import Navbar from "./components/Navbar/Navbar";
 import Auth from "./pages/Auth/Auth.jsx";
@@ -18,30 +18,49 @@ import Dashboard from "./pages/Dashboard/Dashboard.jsx";
 import Loader from "./components/Loader/Loader.jsx";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
   // const { currentUser } = useContext(AuthContext);
   const queryClient = new QueryClient();
 
-  const handleRouteChange = (isLoading) => {
-    setIsLoading(isLoading);
-  };
+  // const Layout = () => {
+  //   return (
+  //     <QueryClientProvider client={queryClient}>
+  //       <Navbar />
+  //       <div style={{display:"flex"}}>
+  //         <Leftbar />
+  //         <div style={{flex:6}}>
+  //           <Outlet />
+  //         </div>
+  //         <Rightbar />
+  //       </div>
+  //       </div>
+  //       </QueryClientProvider>
+  //   )
+  // }
+
+  // const ProtectedRoute = ({ children }) => {
+  //   if (!currentUser) {
+  //     return <Navigate to="/login" />
+  //   }
+
+  //   return children;
+  // }
 
   return (
     <div className="App">
-      <BrowserRouter>
-        {isLoading && <Loader />}
+      {/* <Navbar /> */}
+      {/* <Auth content="signup" /> */}
+      {/* <Dashboard /> */}
+      <Loader />
+      {/* <BrowserRouter>
         <Routes>
-          <Route
-            path="/register"
-            element={<Auth content="signup" handleRouteChange={handleRouteChange} />}
-          />
-          <Route
-            path="/login"
-            element={<Auth content="login" handleRouteChange={handleRouteChange} />}
-          />
-          <Route path="/" element={<Dashboard handleRouteChange={handleRouteChange} />} />
-        </Routes>
-      </BrowserRouter>
+          <Route path="/register" element={<Auth content="signup" />} />
+          <Route path="/login" element={<Auth content="login" />} /> */}
+      {/* <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}> */}
+      {/* <Route path="/" element={<Dashboard />} /> */}
+      {/* <Route path="/profile/:id" element={<Profile />} /> */}
+      {/* </Route> */}
+      {/* </Routes> */}
+      {/* </BrowserRouter> */}
     </div>
   );
 }
