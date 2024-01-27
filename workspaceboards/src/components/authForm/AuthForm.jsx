08718cee58bcb2c5764ed96a89a7f69477d36b8c
sameem786-blip <70144568+sameem786-blip@users.noreply.github.com>
2024-01-27 23:33:10 +0500

    udae: setting u email inut

--- workspaceboards/src/components/authForm/AuthForm.jsx
+++ workspaceboards/src/components/authForm/AuthForm.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import "./authForm.css"
+import "./authForm.css";
+import ForwardIcon from '@mui/icons-material/Forward';
 
 const AuthForm = ({login}) => {
   return (
       <div className='authform-container'>
           <div className="email-input-container">
               <div className="email-input-column-container">
-                  <div className="email-label"></div>
+                  <div className="email-label"><label>Email Address</label></div>
                   <div className="email-input-row-container">
-                      <div className="green-arrow-container"></div>
-                      <div className='input-container'></div>
+                      <div className="green-arrow-container"><ForwardIcon className='arrow-icon' /> </div>
+                      <div className='input-container'><input className='email-input'></input></div>
                   </div>
             </div>
           </div>
