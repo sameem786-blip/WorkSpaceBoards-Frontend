984a6c96f73be69cecaaf4b5d1d7fb817174972e
sameem786-blip <70144568+sameem786-blip@users.noreply.github.com>
2024-01-28 04:07:09 +0500

    update:auth input new comp

--- workspaceboards/src/components/authForm/AuthForm.jsx
+++ workspaceboards/src/components/authForm/AuthForm.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
 import "./authForm.css";
-import ForwardIcon from '@mui/icons-material/Forward';
+import AuthInput from "../AuthInput/AuthInput"
 
 const AuthForm = ({login}) => {
   return (
       <div className='authform-container'>
-          <div className="email-input-container">
-              <div className="email-input-column-container">
-                  <div className="email-label"><label>Email Address</label></div>
-                  <div className="email-input-row-container">
-                      <div className="green-arrow-container"><ForwardIcon className='arrow-icon' /> </div>
-                      <div className='input-container'><input className='email-input'></input></div>
-                  </div>
-            </div>
-          </div>
+          <AuthInput content="email"/>
+          <AuthInput />
     </div>
   )
 }
