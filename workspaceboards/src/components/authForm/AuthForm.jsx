526a4bf3feabd774f154c200e78ac253a42017a4
Sameem Abbas <sameemabbas2002@outlook.com>
2024-01-29 14:12:02 +0500

    i dont know what i did

--- workspaceboards/src/components/authForm/AuthForm.jsx
+++ workspaceboards/src/components/authForm/AuthForm.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./authForm.css";
-import AuthInput from "../AuthInput/AuthInput"
-import ForwardIcon from '@mui/icons-material/Forward';
+import AuthInput from "../AuthInput/AuthInput";
+import ForwardIcon from "@mui/icons-material/Forward";
 
-const AuthForm = ({login}) => {
+const AuthForm = ({ login }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
-      <div className='authform-container'>
-          <AuthInput content="email"/>
-          <AuthInput />
-          <div className="btn-container">
-              <div className="btn-row">
-                  <div className="green-arrow-container"><ForwardIcon className='arrow-icon' /></div>
-                    <button className='btn'>Submit</button>
-              </div>
+    <div className="authform-container">
+      <AuthInput content="email" onChange={handleEmailChange} />
+      <AuthInput content="password" onChange={handlePasswordChange} />
+      <div className="btn-container">
+        <div className="btn-row">
+          <div className="green-arrow-container">
+            <ForwardIcon />
           </div>
+          <button className="btn">{login ? "Login" : "Sign up"}</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthForm
+export default AuthForm;
