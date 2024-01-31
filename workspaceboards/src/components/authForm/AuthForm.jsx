033123199e5e03793d9c176773b455bec6e5deea
sameem786-blip <70144568+sameem786-blip@users.noreply.github.com>
2024-01-31 19:49:19 +0500

    fix: auth redirection 1

--- workspaceboards/src/components/authForm/AuthForm.jsx
+++ workspaceboards/src/components/authForm/AuthForm.jsx
@@ -6,6 +6,7 @@ import ForwardIcon from "@mui/icons-material/Forward";
 const AuthForm = ({ login }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [signUpStage, setSignupStage]= useState(false)
 
   const handleEmailChange = (e) => {
     e.preventDefault();
@@ -19,14 +20,25 @@ const AuthForm = ({ login }) => {
     console.log("password: ", password);
   };
 
+  const handleSignupRedirect = () => {
+setSignupStage(true)
+  }
+
+  const handleLogin = () => {
+
+  }
+  const handleSignup = () => {
+
+  }
+
   return (
     <div className="authform-container">
       <AuthInput content="email" callback={handleEmailChange} />
       <AuthInput content="password" callback={handlePasswordChange} />
       <div className="btn-container">
         <div className="btn-row">
-          <button className="btn">
-            {email ? (password ? "Sign In" : "Sign Up") : "Sign Up"}
+          <button className="btn" onClick={email ? (password ? (handleLogin) : (signUpStage ? (handleSignup) : (handleSignupRedirect))) : (signUpStage ? (handleSignup) : (handleSignupRedirect))}>
+            {email ? (password ? (signUpStage ? "Sign Up" : "Sign In") : "Sign Up") : "Sign Up"}
           </button>
         </div>
       </div>
