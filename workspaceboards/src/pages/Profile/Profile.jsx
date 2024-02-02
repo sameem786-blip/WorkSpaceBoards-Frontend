8f021bcd6626ab22c750cb00785d265c7c3bd4a1
sameem786-blip <70144568+sameem786-blip@users.noreply.github.com>
2024-02-03 00:04:56 +0500

    lklm

--- workspaceboards/src/pages/Profile/Profile.jsx
+++ workspaceboards/src/pages/Profile/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./profile.css"
 
 import Hellboy from '../../components/Hellboy/Hellboy'
+import BorderColorIcon from '@mui/icons-material/BorderColor';
 
 const Profile = () => {
   return (
@@ -13,7 +14,7 @@ const Profile = () => {
         <div className="right-section">
           <img src="https://cdn.dribbble.com/users/2878951/screenshots/14013747/media/603f0b853c409547dfa51cba996f375c.png?resize=1000x750&vertical=center" className='display-picture' />
             <div className="text-row">
-              <p className='text-row-entry'>username: `<span className='text-light'>changazi786</span>`</p>
+              <p className='text-row-entry'>@username: `<span className='text-light'>changazi786</span>`</p><BorderColorIcon className='edit-icon'/>
             </div>
             <div className="text-row">
               <p className='text-row-entry'>First-name: `<span className='text-light'>changazi786</span>`</p>
