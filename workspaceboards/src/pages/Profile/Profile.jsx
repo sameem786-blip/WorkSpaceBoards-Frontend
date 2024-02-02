610e96705a9459d96cafb76814090193b828ade7
sameem786-blip <70144568+sameem786-blip@users.noreply.github.com>
2024-02-03 00:11:03 +0500

    lkmmklm

--- workspaceboards/src/pages/Profile/Profile.jsx
+++ workspaceboards/src/pages/Profile/Profile.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import {useContext} from 'react'
+import { AuthContext } from "../../context/authContext";
 import "./profile.css"
 
 import Hellboy from '../../components/Hellboy/Hellboy'
 import BorderColorIcon from '@mui/icons-material/BorderColor';
 
 const Profile = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className='profile-container'>
       <div className="profileCard">
@@ -14,16 +16,16 @@ const Profile = () => {
         <div className="right-section">
           <img src="https://cdn.dribbble.com/users/2878951/screenshots/14013747/media/603f0b853c409547dfa51cba996f375c.png?resize=1000x750&vertical=center" className='display-picture' />
             <div className="text-row">
-              <p className='text-row-entry'>@username: `<span className='text-light'>changazi786</span>`</p><BorderColorIcon className='edit-icon'/>
+            <p className='text-row-entry'>@username: `<span className='text-light'>{ currentUser.user.username}</span>`</p><BorderColorIcon className='edit-icon'/>
             </div>
             <div className="text-row">
-              <p className='text-row-entry'>Email: `<span className='text-light'>changazi786</span>`</p>
+              <p className='text-row-entry'>Email: `<span className='text-light'>{currentUser.user.email}</span>`</p>
             </div>
             <div className="text-row">
-              <p className='text-row-entry'>First-name: `<span className='text-light'>changazi786</span>`</p>
+              <p className='text-row-entry'>First-name: `<span className='text-light'>{currentUser.user.firstName}</span>`</p>
             </div>
             <div className="text-row">
-              <p className='text-row-entry'>Last-name: `<span className='text-light'>changazi786</span>`</p>
+              <p className='text-row-entry'>Last-name: `<span className='text-light'>{currentUser.user.lastName}</span>`</p>
             </div>
             <div className="text-row">
               <button className="profile-action">Reset Password</button>
