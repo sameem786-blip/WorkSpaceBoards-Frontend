955fea01d3d0eddd6077c249a1a25181001acf98
sameem786-blip <70144568+sameem786-blip@users.noreply.github.com>
2024-02-02 22:54:19 +0500

    reset

--- workspaceboards/src/pages/Profile/Profile.jsx
+++ workspaceboards/src/pages/Profile/Profile.jsx
@@ -6,18 +6,7 @@ import Hellboy from '../../components/Hellboy/Hellboy'
 const Profile = () => {
   return (
     <div className='profile-container'>
-      <div className="hellboy-container">
-        <div className="profileCard">
-          
-      <Hellboy />
-        </div>
-
-      </div>
-      <div className="profile-inner-container">
-        <div className="profileCard">
-
-        </div>
-      </div>
+      
     </div>
   )
 }
