9d87ceb189d5133e601659b21593054b50be91e6
sameem786-blip <70144568+sameem786-blip@users.noreply.github.com>
2024-02-02 23:11:45 +0500

    kjjnbkj

--- workspaceboards/src/pages/Profile/Profile.jsx
+++ workspaceboards/src/pages/Profile/Profile.jsx
@@ -7,8 +7,24 @@ const Profile = () => {
   return (
     <div className='profile-container'>
       <div className="profileCard">
-        <div className="left-section"></div>
-        <div className="right-section"></div>
+        <div className="left-section">
+          <Hellboy />
+        </div>
+        <div className="right-section">
+          <img src="https://cdn.dribbble.com/users/2878951/screenshots/14013747/media/603f0b853c409547dfa51cba996f375c.png?resize=1000x750&vertical=center" className='display-picture' />
+            <div className="text-row">
+              <p className='text-row-entry'>username: `changazi786`</p>
+            </div>
+            <div className="text-row">
+              <p className='text-row-entry'>First-name: `changazi786`</p>
+            </div>
+            <div className="text-row">
+              <p className='text-row-entry'>Last-name: `changazi786`</p>
+            </div>
+            <div className="text-row">
+              <p className='text-row-entry'>Email: `changazi786`</p>
+            </div>
+        </div>
       </div>
     </div>
   )
