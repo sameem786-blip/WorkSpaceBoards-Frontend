497b95c599ed17587757062bde576009970f607b
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-05 22:57:05 +0500

    board created

--- workspaceboards/src/components/OverlayPopup/OverlayPopup.jsx
+++ workspaceboards/src/components/OverlayPopup/OverlayPopup.jsx
@@ -4,7 +4,10 @@ import "./overlaypopup.css"
 const OverlayPopup = () => {
   return (
       <div className='popup-container'>
-          
+          <div className="popup-board">
+              <div className="left-kiki"></div>
+              <div className="right-message"></div>
+          </div>
     </div>
   )
 }
