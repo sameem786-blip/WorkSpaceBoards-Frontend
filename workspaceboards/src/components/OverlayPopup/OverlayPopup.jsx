ea1d380d70469d06cdf72a27cb1954b1a85b9f05
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-05 23:03:09 +0500

    plan changed

--- workspaceboards/src/components/OverlayPopup/OverlayPopup.jsx
+++ workspaceboards/src/components/OverlayPopup/OverlayPopup.jsx
@@ -4,10 +4,10 @@ import "./overlaypopup.css"
 const OverlayPopup = () => {
   return (
       <div className='popup-container'>
-          <div className="popup-board">
-              <div className="left-kiki"></div>
-              <div className="right-message"></div>
-          </div>
+          <div className="top-plank"></div>
+          <div className="middle-plank"></div>
+          <div className="bottom-plank"></div>
+          <div className="pole"></div>
     </div>
   )
 }
