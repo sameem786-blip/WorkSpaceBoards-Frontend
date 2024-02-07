2033c290108fe7e190d1fa0057fa1310fb5e1864
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-07 20:38:00 +0500

    click func on edit

--- workspaceboards/src/pages/Profile/Profile.jsx
+++ workspaceboards/src/pages/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import {useContext} from 'react'
+import {useContext, useState} from 'react'
 import { AuthContext } from "../../context/authContext";
 import "./profile.css"
 
@@ -7,6 +7,11 @@ import BorderColorIcon from '@mui/icons-material/BorderColor';
 
 const Profile = () => {
   const { currentUser } = useContext(AuthContext);
+  const { editProfile, setEditProfile } = useState(false)
+  
+  const handleEditProfileClick = () => {
+    setEditProfile(true)
+  }
   return (
     <div className='profile-container'>
       <div className="profileCard">
@@ -16,16 +21,16 @@ const Profile = () => {
         <div className="right-section">
           <img src="https://cdn.dribbble.com/users/2878951/screenshots/14013747/media/603f0b853c409547dfa51cba996f375c.png?resize=1000x750&vertical=center" className='display-picture' />
             <div className="text-row">
-            <p className='text-row-entry'>@username: `<span className='text-light'>{ currentUser.user.username}</span>`</p><BorderColorIcon className='edit-icon'/>
+            <p className='text-row-entry'>@username: `<span className='text-light'>{currentUser.user.username}</span>`</p><BorderColorIcon onClick={handleEditProfileClick} className='edit-icon'/>
             </div>
             <div className="text-row">
               <p className='text-row-entry'>Email: `<span className='text-light'>{currentUser.user.email}</span>`</p>
             </div>
             <div className="text-row">
-              <p className='text-row-entry'>First-name: `<span className='text-light'>{currentUser.user.firstName}</span>`</p><BorderColorIcon className='edit-icon'/>
+              <p className='text-row-entry'>First-name: `<span className='text-light'>{currentUser.user.firstName}</span>`</p><BorderColorIcon onClick={handleEditProfileClick} className='edit-icon'/>
             </div>
             <div className="text-row">
-              <p className='text-row-entry'>Last-name: `<span className='text-light'>{currentUser.user.lastName}</span>`</p><BorderColorIcon className='edit-icon'/>
+              <p className='text-row-entry'>Last-name: `<span className='text-light'>{currentUser.user.lastName}</span>`</p><BorderColorIcon onClick={handleEditProfileClick} className='edit-icon'/>
             </div>
             <div className="text-row">
               <button className="profile-action">Reset Password</button>
