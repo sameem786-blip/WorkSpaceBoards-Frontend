ad2dfc1f0ba5b58d81714eb120bd5dc18b905e61
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-13 00:29:57 +0500

    nkl

--- workspaceboards/src/components/OTPInput/OtpInput.jsx
+++ workspaceboards/src/components/OTPInput/OtpInput.jsx
@@ -4,6 +4,9 @@ import "./otpInput.css"
 const OtpInput = () => {
   return (
     <div className='otp-container'>
+      <div className='otp-msg'>
+        <p className='otp-label'>A 4 digit otp has been sent to your email, enter it below to complete the verification process and move ahead.</p>
+      </div>
       <div className="otp-input-row">
         <input className='otp-input'></input>
         <input className='otp-input'></input>
