edc10f3f05e1b02771480601211ab5f104950476
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-13 00:27:11 +0500

    njkdc

--- workspaceboards/src/pages/Profile/Profile.jsx
+++ workspaceboards/src/pages/Profile/Profile.jsx
@@ -128,8 +128,10 @@ response = await axios.put(`${serverURL}/api/user/updateLastname`, {
               <button className="profile-action" onClick={handleUpdateClick}>Update {edit }</button>
             </div>
           </div>
-        ) : (
-          <div className="right-section">
+        ) : (passwordResetState ? (
+        <OtpInput />
+        ): (
+            <div className="right-section">
           <img src="https://cdn.dribbble.com/users/2878951/screenshots/14013747/media/603f0b853c409547dfa51cba996f375c.png?resize=1000x750&vertical=center" className='display-picture' />
             <div className="text-row">
             <p className='text-row-entry'>@username: `<span className='text-light'>{currentUser.user.username}</span>`</p><BorderColorIcon onClick={() => handleEditProfileClick("Username")} className='edit-icon'/>
@@ -147,6 +149,7 @@ response = await axios.put(`${serverURL}/api/user/updateLastname`, {
                 <button className="profile-action" onClick={handlePasswordResetStateChange}>Reset Password</button>
             </div>
         </div>  
+        )
         )}
         
       </div>
