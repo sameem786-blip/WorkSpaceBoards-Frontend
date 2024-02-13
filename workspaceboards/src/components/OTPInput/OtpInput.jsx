f3375ec6e62846246b9f4129817f7bbeb73828cb
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-14 00:32:07 +0500

    lklknln

--- workspaceboards/src/components/OTPInput/OtpInput.jsx
+++ workspaceboards/src/components/OTPInput/OtpInput.jsx
@@ -8,10 +8,10 @@ const OtpInput = () => {
         <p className='otp-label'>A 4 digit otp has been sent to your email, enter it below to complete the verification process and move ahead.</p>
       </div>
       <div className="otp-input-row">
-        <input className='otp-input'></input>
-        <input className='otp-input'></input>
-        <input className='otp-input'></input>
-        <input className='otp-input'></input>
+        <input className='otp-input' type="number" maxlength="1" min="0" max="9" step="1"></input>
+        <input className='otp-input'type="number" maxlength="1" min="0" max="9" step="1"></input>
+        <input className='otp-input'type="number" maxlength="1" min="0" max="9" step="1"></input>
+        <input className='otp-input'type="number" maxlength="1" min="0" max="9" step="1"></input>
       </div>
     </div>
   )
