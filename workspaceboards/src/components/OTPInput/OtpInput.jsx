bc313e8b6e89b8df80251516f4921e8fc1c9c629
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-14 01:15:32 +0500

    iomom

--- workspaceboards/src/components/OTPInput/OtpInput.jsx
+++ workspaceboards/src/components/OTPInput/OtpInput.jsx
@@ -1,45 +1,57 @@
-import React,{useState} from 'react'
-import "./otpInput.css"
+import React, { useState } from 'react';
+import "./otpInput.css";
 
 const OtpInput = () => {
-  const [otp1, setOtp1] = useState("")
-  const [otp2, setOtp2] = useState("")
-  const [otp3, setOtp3] = useState("")
-  const [otp4, setOtp4] = useState("")
+  const [otp1, setOtp1] = useState("");
+  const [otp2, setOtp2] = useState("");
+  const [otp3, setOtp3] = useState("");
+  const [otp4, setOtp4] = useState("");
 
-  const handleOtpInput = async (e) => {
-    if (e.target.name == "otp1") {
-      setOtp1(e.target.value)
-    } else if (e.target.name == "otp2") {
-      setOtp2(e.target.value)
+  const handleOtpInput = (e) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case "otp1":
+        setOtp1(value);
+        if (value !== "") document.getElementsByName("otp2")[0].focus();
+        break;
+      case "otp2":
+        setOtp2(value);
+        if (value !== "") document.getElementsByName("otp3")[0].focus();
+        break;
+      case "otp3":
+        setOtp3(value);
+        if (value !== "") document.getElementsByName("otp4")[0].focus();
+        break;
+      case "otp4":
+        setOtp4(value);
+        break;
+      default:
+        break;
     }
-    else if (e.target.name == "otp3") {
-      setOtp3(e.target.value)
-    }
-    else {
-      setOtp4(e.target.value)
-    }
-  }
+  };
 
-  const submitOtp = async () => {
-    
-  }
+  const submitOtp = () => {
+    const otp = otp1 + otp2 + otp3 + otp4;
+    // Implement your OTP submission logic here
+    console.log("OTP submitted:", otp);
+  };
+
   return (
     <div className='otp-container'>
       <div className='otp-msg'>
         <p className='otp-label'>A 4 digit otp has been sent to your email, enter it below to complete the verification process and move ahead.</p>
       </div>
       <div className="otp-input-row">
-        <input className='otp-input' value={otp1} name="otp1" type="number" onChange={ handleOtpInput} maxlength="1" min="0" max="9" step="1"></input>
-        <input className='otp-input' value={ otp1} name="otp2" type="number" onChange={ handleOtpInput} maxlength="1" min="0" max="9" step="1"></input>
-        <input className='otp-input' value={ otp1} name="otp3" type="number"  onChange={ handleOtpInput} maxlength="1" min="0" max="9" step="1"></input>
-        <input className='otp-input' value={ otp1} name="otp4" type="number" onChange={ handleOtpInput} maxlength="1" min="0" max="9" step="1"></input>
+        <input className='otp-input' value={otp1} name="otp1" type="number" onChange={handleOtpInput} maxLength="1" min="0" max="9" step="1" />
+        <input className='otp-input' value={otp2} name="otp2" type="number" onChange={handleOtpInput} maxLength="1" min="0" max="9" step="1" />
+        <input className='otp-input' value={otp3} name="otp3" type="number" onChange={handleOtpInput} maxLength="1" min="0" max="9" step="1" />
+        <input className='otp-input' value={otp4} name="otp4" type="number" onChange={handleOtpInput} maxLength="1" min="0" max="9" step="1" />
       </div>
       <div className='otp-submit'>
-        <button className='btn'>Submit</button>
+        <button className='btn' onClick={submitOtp}>Submit</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default OtpInput
+export default OtpInput;
