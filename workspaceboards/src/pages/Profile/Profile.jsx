a7821bf42d3f281de2e9a031a158f836c7af5754
Sameem Abbas <sameemabbas2002@outlook.com>
2024-02-16 18:41:59 +0500

    klnkl

--- workspaceboards/src/pages/Profile/Profile.jsx
+++ workspaceboards/src/pages/Profile/Profile.jsx
@@ -1,12 +1,13 @@
-import {useContext, useState} from 'react'
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
-import "./profile.css"
+import "./profile.css";
 import axios from "axios";
 
-import Hellboy from '../../components/Hellboy/Hellboy'
-import BorderColorIcon from '@mui/icons-material/BorderColor';
-import AuthInput from '../../components/AuthInput/AuthInput';
-import OtpInput from '../../components/OTPInput/OtpInput';
+import Hellboy from "../../components/Hellboy/Hellboy";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+import AuthInput from "../../components/AuthInput/AuthInput";
+import OtpInput from "../../components/OTPInput/OtpInput";
+import OverlayPopup from "../../components/OverlayPopup/OverlayPopup";
 
 import { Link, useNavigate } from "react-router-dom";
 
@@ -16,165 +17,223 @@ const serverURL = config.serverURL;
 
 const Profile = () => {
   const { currentUser } = useContext(AuthContext);
-  const [err, setErr] = useState(false)
-  const [passwordResetState, setPasswordResetState] = useState(false)
-  const [ editProfile, setEditProfile ] = useState(false)
-  const [ edit, setEdit ] = useState("")
-  const [ newUsername, setNewUsername ] = useState("")
-  const [ newFirstname, setNewFirstname ] = useState("")
-  const [ newLastname, setNewLastname ] = useState("")
-  
+  const [err, setErr] = useState(false);
+  const [passwordResetState, setPasswordResetState] = useState(false);
+  const [editProfile, setEditProfile] = useState(false);
+  const [edit, setEdit] = useState("");
+  const [newUsername, setNewUsername] = useState("");
+  const [newFirstname, setNewFirstname] = useState("");
+  const [newLastname, setNewLastname] = useState("");
+
   const [toChange, setToChange] = useState("");
 
   const navigate = useNavigate();
-  
+
   const handleEditProfileClick = (edit) => {
     setEditProfile(true);
 
-    console.log(edit)
-    setEdit(edit)
-    console.log(editProfile)
-  }
+    console.log(edit);
+    setEdit(edit);
+    console.log(editProfile);
+  };
 
   const handleNewChange = (e) => {
     if (edit == "Username") {
       e.preventDefault();
-      setNewUsername(e.target.value)
-    }
-    else if (edit == "First Name") {
+      setNewUsername(e.target.value);
+    } else if (edit == "First Name") {
       e.preventDefault();
-      setNewFirstname(e.target.value)
-    }
-    else if (edit == "Last Name") {
+      setNewFirstname(e.target.value);
+    } else if (edit == "Last Name") {
       e.preventDefault();
-      setNewLastname(e.target.value)
+      setNewLastname(e.target.value);
     } else {
       return;
     }
-  }
+  };
 
   const handleEntry = (e) => {
-    setToChange(e.target.value)
-  }
+    setToChange(e.target.value);
+  };
 
-  const handleUpdateClick = async() => {
+  const handleUpdateClick = async () => {
     try {
-      let response
+      let response;
       if (edit == "Username") {
-        response = await axios.put(`${serverURL}/api/user/updateUsername`, {
-          username: toChange,
-        },
-          {headers: {
-          Authorization: `Bearer ${currentUser.token}`,
-        }}
+        response = await axios.put(
+          `${serverURL}/api/user/updateUsername`,
+          {
+            username: toChange,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${currentUser.token}`,
+            },
+          }
         );
 
-        currentUser.user.username = toChange
-        setEditProfile(false)
-        setEdit("")
-      }
-      else if (edit == "First Name") {
-      response = await axios.put(`${serverURL}/api/user/updateFirstname`, {
-          firstName: toChange,
-        },{headers: {
-          Authorization: `Bearer ${currentUser.token}`,
-        }});
+        currentUser.user.username = toChange;
+        setEditProfile(false);
+        setEdit("");
+      } else if (edit == "First Name") {
+        response = await axios.put(
+          `${serverURL}/api/user/updateFirstname`,
+          {
+            firstName: toChange,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${currentUser.token}`,
+            },
+          }
+        );
 
-        currentUser.user.firstName = toChange
+        currentUser.user.firstName = toChange;
         setEditProfile(false);
-        setEdit("")
-      }
-      else if (edit == "Last Name") {
-response = await axios.put(`${serverURL}/api/user/updateLastname`, {
-          lastName: toChange,
-        },{headers: {
-          Authorization: `Bearer ${currentUser.token}`,
-}
-});
-        
-        currentUser.user.lastName = toChange
+        setEdit("");
+      } else if (edit == "Last Name") {
+        response = await axios.put(
+          `${serverURL}/api/user/updateLastname`,
+          {
+            lastName: toChange,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${currentUser.token}`,
+            },
+          }
+        );
+
+        currentUser.user.lastName = toChange;
         setEditProfile(false);
-        setEdit("")
+        setEdit("");
       } else {
         return;
       }
 
       localStorage.setItem("workspace-user", JSON.stringify(currentUser));
-      window.location.reload(); 
-    } catch (err) {
-      
-    }
-  }
+      window.location.reload();
+    } catch (err) {}
+  };
 
-  const handlePasswordResetStateChange = async() => {
+  const handlePasswordResetStateChange = async () => {
     setPasswordResetState(true);
     try {
-      const response = await axios.post(`${serverURL}/auth/user/forgetPassword/sendOTP`, {
-        email: currentUser.user.email
-      });
+      const response = await axios.post(
+        `${serverURL}/auth/user/forgetPassword/sendOTP`,
+        {
+          email: currentUser.user.email,
+        }
+      );
 
       if (response.status == 200) {
         setPasswordResetState(true);
       } else {
         window.location.reload();
       }
-
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
   return (
-    <div className='profile-container'>
+    <div className="profile-container">
       <div className="profileCard">
         <div className="left-section">
-          {err ? () : (
-
-            <Hellboy />
-          )}
+          {err ? <OverlayPopup /> : <Hellboy />}
         </div>
-        {editProfile ?(
+        {editProfile ? (
           <div className="right-section">
-            <img src="https://cdn.dribbble.com/users/2878951/screenshots/14013747/media/603f0b853c409547dfa51cba996f375c.png?resize=1000x750&vertical=center" className='display-picture' />
+            <img
+              src="https://cdn.dribbble.com/users/2878951/screenshots/14013747/media/603f0b853c409547dfa51cba996f375c.png?resize=1000x750&vertical=center"
+              className="display-picture"
+            />
             <div className="text-row">
-              <p className='text-row-entry'>{ edit}: `<span className='text-light'>{edit == "Username" ? (currentUser.user.username) : (edit == "First Name" ? (currentUser.user.firstName) : (currentUser.user.lastName))}</span>`</p>
+              <p className="text-row-entry">
+                {edit}: `
+                <span className="text-light">
+                  {edit == "Username"
+                    ? currentUser.user.username
+                    : edit == "First Name"
+                    ? currentUser.user.firstName
+                    : currentUser.user.lastName}
+                </span>
+                `
+              </p>
             </div>
             <div className="text-column">
-              <label className='profile-input-label'>New {edit}</label>
-              <input className='profile-input' value={toChange} onChange={handleEntry}></input>
+              <label className="profile-input-label">New {edit}</label>
+              <input
+                className="profile-input"
+                value={toChange}
+                onChange={handleEntry}
+              ></input>
             </div>
             <div className="text-row">
-              <button className="profile-action" onClick={handleUpdateClick}>Update {edit }</button>
+              <button className="profile-action" onClick={handleUpdateClick}>
+                Update {edit}
+              </button>
             </div>
           </div>
-        ) : (passwordResetState ? (
-            <div className="right-section">
-              <OtpInput />
-              </div>
-        ): (
-            <div className="right-section">
-          <img src="https://cdn.dribbble.com/users/2878951/screenshots/14013747/media/603f0b853c409547dfa51cba996f375c.png?resize=1000x750&vertical=center" className='display-picture' />
+        ) : passwordResetState ? (
+          <div className="right-section">
+            <OtpInput />
+          </div>
+        ) : (
+          <div className="right-section">
+            <img
+              src="https://cdn.dribbble.com/users/2878951/screenshots/14013747/media/603f0b853c409547dfa51cba996f375c.png?resize=1000x750&vertical=center"
+              className="display-picture"
+            />
             <div className="text-row">
-            <p className='text-row-entry'>@username: `<span className='text-light'>{currentUser.user.username}</span>`</p><BorderColorIcon onClick={() => handleEditProfileClick("Username")} className='edit-icon'/>
+              <p className="text-row-entry">
+                @username: `
+                <span className="text-light">{currentUser.user.username}</span>`
+              </p>
+              <BorderColorIcon
+                onClick={() => handleEditProfileClick("Username")}
+                className="edit-icon"
+              />
             </div>
             <div className="text-row">
-              <p className='text-row-entry'>Email: `<span className='text-light'>{currentUser.user.email}</span>`</p>
+              <p className="text-row-entry">
+                Email: `
+                <span className="text-light">{currentUser.user.email}</span>`
+              </p>
             </div>
             <div className="text-row">
-            <p className='text-row-entry'>First-name: `<span className='text-light'>{currentUser.user.firstName}</span>`</p><BorderColorIcon onClick={() => handleEditProfileClick("First Name")} className='edit-icon'/>
+              <p className="text-row-entry">
+                First-name: `
+                <span className="text-light">{currentUser.user.firstName}</span>
+                `
+              </p>
+              <BorderColorIcon
+                onClick={() => handleEditProfileClick("First Name")}
+                className="edit-icon"
+              />
             </div>
             <div className="text-row">
-              <p className='text-row-entry'>Last-name: `<span className='text-light'>{currentUser.user.lastName}</span>`</p><BorderColorIcon onClick={() => handleEditProfileClick("Last Name")} className='edit-icon'/>
+              <p className="text-row-entry">
+                Last-name: `
+                <span className="text-light">{currentUser.user.lastName}</span>`
+              </p>
+              <BorderColorIcon
+                onClick={() => handleEditProfileClick("Last Name")}
+                className="edit-icon"
+              />
             </div>
             <div className="text-row">
-                <button className="profile-action" onClick={handlePasswordResetStateChange}>Reset Password</button>
+              <button
+                className="profile-action"
+                onClick={handlePasswordResetStateChange}
+              >
+                Reset Password
+              </button>
             </div>
-        </div>  
-        )
+          </div>
         )}
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
