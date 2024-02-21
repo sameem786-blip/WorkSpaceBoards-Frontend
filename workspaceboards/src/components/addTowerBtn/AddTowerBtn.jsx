74c51409f003772c5714a030439e6bb6bab9eda0
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-22 00:00:10 +0500

    klnkln

--- workspaceboards/src/components/addTowerBtn/AddTowerBtn.jsx
+++ workspaceboards/src/components/addTowerBtn/AddTowerBtn.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const AddTowerBtn = () => {
   return (
       <div className='addtowerbtn-container'>
-          AddTowerBtn
+      <div className="addtower-opaque">
+        
+          </div>
     </div>
   )
 }
