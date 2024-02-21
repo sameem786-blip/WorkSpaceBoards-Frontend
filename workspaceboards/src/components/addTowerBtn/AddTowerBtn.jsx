8f2eb3c2a911aed0272561c3c2a3472ab3b6a403
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-22 00:14:09 +0500

    lknlk

--- workspaceboards/src/components/addTowerBtn/AddTowerBtn.jsx
+++ workspaceboards/src/components/addTowerBtn/AddTowerBtn.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./addtowerbtn.css"
+import builder from "../../public/Icons/builder"
 
 const AddTowerBtn = () => {
   return (
       <div className='addtowerbtn-container'>
       <div className="addtower-opaque">
-        
+        <img src={builder} alt='builder'></img>
           </div>
     </div>
   )
