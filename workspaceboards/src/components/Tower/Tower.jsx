c60ab341d7616a5979d4e29c0b942860002e21b5
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-23 00:14:23 +0500

    d d d

--- workspaceboards/src/components/Tower/Tower.jsx
+++ workspaceboards/src/components/Tower/Tower.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./tower.css"
+import towerTop from "../../public/towwer/top.png"
+import towerBottom from "../../public/towwer/bottom.png"
 
 const Tower = () => {
   return (
       <div className='tower-container'>
-          Tower
+      <div className="tower-top">
+        <img src={towerTop} alt='top' className='tower-top'/>
+          </div>
+          <div className="tower-center"></div>
+      <div className="tower-bottom">
+        <img src={towerBottom } alt='top' className='tower-bottom'/>
+          </div>
     </div>
   )
 }
