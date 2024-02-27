f44eb1b4ca1ecbf01a2a6e0f4d067f49eabdc92e
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-27 23:27:09 +0500

    ger

--- workspaceboards/src/components/Card/Card.jsx
+++ workspaceboards/src/components/Card/Card.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import "./card.css";
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import CommentIcon from '@mui/icons-material/Comment';
+import CrisisAlertIcon from '@mui/icons-material/CrisisAlert';
 
 const Card = () => {
   return (
       <div className='card-container'>
           <div className="car-title">Card Container Title</div>
-          <div className="card-icons">
-              <VisibilityIcon className='class-icon'/>
-              <CommentIcon />
+          <div className="card-icons-container">
+              <VisibilityIcon className='class-icon'/>3
+              <CommentIcon className='class-icon'/>3
+              <CrisisAlertIcon className='class-icon important'/>
           </div>
     </div>
   )
