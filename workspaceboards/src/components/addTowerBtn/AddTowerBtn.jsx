9bf27aeb32971e229d633b243787fcede23b3876
SameemAbbas <sameemabbas2002@outlook.com>
2024-02-28 12:47:00 +0500

    klnlk

--- workspaceboards/src/components/addTowerBtn/AddTowerBtn.jsx
+++ workspaceboards/src/components/addTowerBtn/AddTowerBtn.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./addtowerbtn.css"
 import builder from "../../public/Icons/builder.png"
+import letterman from "../../public/Card/abc.png"
 
 const AddTowerBtn = () => {
   return (
@@ -10,7 +11,7 @@ const AddTowerBtn = () => {
         <p className='add-tower-text'>Add Tower</p>
           </div>
       <div className="addtower-opaque">
-        <img src={builder} alt='builder' className='builder-icon'></img>
+        <img src={letterman} alt='builder' className='builder-icon'></img>
         <p className='add-tower-text'>Add Card</p>
           </div>
     </div>
