05ad1ea34faaf3be7655c717b823fc470927ce51
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-01 00:24:36 +0500

    fb

--- workspaceboards/src/pages/Dashboard/Dashboard.jsx
+++ workspaceboards/src/pages/Dashboard/Dashboard.jsx
@@ -9,9 +9,56 @@ const Dashboard = () => {
 
   const towers = [
     {
+      "id": "1",
       "name": "Tower 1",
       "createdById": "1",
-      "position": "1"
+      "position": "1",
+      "cards": [
+        {
+          "id": "1",
+          "position": "1",
+          "title": "Test Title 1",
+          
+        },
+        {
+          "id": "2",
+          "position": "1",
+          "title": "Test Title 1",
+
+        },
+        {
+          "id": "3",
+          "position": "1",
+          "title": "Test Title 1",
+
+        }
+      ],
+    },
+    {
+      "id": "2",
+      "name": "Tower 3",
+      "createdById": "1",
+      "position": "2",
+      "cards": [
+        {
+          "id": "4",
+          "position": "1",
+          "title": "Test Title 1",
+          
+        },
+        {
+          "id": "2",
+          "position": "1",
+          "title": "Test Title 1",
+
+        },
+        {
+          "id": "3",
+          "position": "1",
+          "title": "Test Title 1",
+
+        }
+      ],
     },
   ]
   return (
