47c07b964ef1c27c37cc5d5f9d329e1a15775b07
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-01 00:44:19 +0500

    dfvdf

--- workspaceboards/src/components/Tower/Tower.jsx
+++ workspaceboards/src/components/Tower/Tower.jsx
@@ -5,14 +5,17 @@ import towerBottom from "../../public/towwer/bottom.png"
 import Card from '../Card/Card'
 
 
-const Tower = ({tower}) => {
+const Tower = ({ tower }) => {
+  const cards = tower.cards;
   return (
       <div className='tower-container'>
       <div className="tower-top">
         <img src={towerTop} alt='top' className='top-img'/>
           </div>
       <div className="tower-center">
-        <Card />
+        {cards.map((item,index) => (
+          <Card cards={cards} key={ index} />
+      ))}
       </div>
       <div className="tower-bottom">
         <img src={towerBottom } alt='top' className='bottom-img'/>
