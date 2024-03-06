bc8476e991805cda7fc0ca477f18a25e1ad48171
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-06 23:10:18 +0500

    lkmlk

--- workspaceboards/src/components/Tower/Tower.jsx
+++ workspaceboards/src/components/Tower/Tower.jsx
@@ -15,22 +15,50 @@ import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
 
 
 const Tower = ({ tower }) => {
-  const cards = tower.cards;
-  return (
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const getTaskPos = (id) => tasks.findIndex((task) => task.id === id);
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+
+    if (active.id === over.id) return;
+
+    setTasks((tasks) => {
+      const originalPos = getTaskPos(active.id);
+      const newPos = getTaskPos(over.id);
+
+      return arrayMove(tasks, originalPos, newPos);
+    });
+    const cards = tower.cards;
+    return (
       <div className='tower-container'>
-      <div className="tower-top">
-        <img src={towerTop} alt='top' className='top-img'/>
-          </div>
-      <div className="tower-center">
-        {cards.map((item,index) => (
-          <Card card={item} key={ index} />
-      ))}
+        <div className="tower-top">
+          <img src={towerTop} alt='top' className='top-img' />
+        </div>
+        <div className="tower-center">
+          <DndContext
+            sensors={sensors}
+            collisionDetection={closestCorners}
+            onDragEnd={handleDragEnd}
+          >
+            {cards.map((item, index) => (
+              <Card card={item} key={index} />
+            ))}
+          </DndContext>
+        </div>
+        <div className="tower-bottom">
+          <img src={towerBottom} alt='top' className='bottom-img' />
+        </div>
       </div>
-      <div className="tower-bottom">
-        <img src={towerBottom } alt='top' className='bottom-img'/>
-          </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Tower
