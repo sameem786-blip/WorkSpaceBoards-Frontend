12f87a7aab15de4114f3ccccb3b9e917407e4f43
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-13 00:13:11 +0500

    nlk

--- workspaceboards/src/components/Card/Card.jsx
+++ workspaceboards/src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ import CommentIcon from '@mui/icons-material/Comment';
 import CrisisAlertIcon from '@mui/icons-material/CrisisAlert';
 import LaunchIcon from '@mui/icons-material/Launch';
 import { useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities"jfdvbkjbk
+import { CSS } from "@dnd-kit/utilities"
 const Card = ({ id, title, comments, createdBy }) => {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id });
