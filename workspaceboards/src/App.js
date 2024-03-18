f804a41347446fc1b2b1152a872ae21ea7c5ca03
SameemAbbas <sameemabbas2002@outlook.com>
2024-03-18 20:11:11 +0500

    vd

--- workspaceboards/src/App.js
+++ workspaceboards/src/App.js
@@ -10,7 +10,7 @@ import {
   Outlet,
   Route,
   Routes,
-} from "react-router-dom";
+} from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import AuthPage from "./pages/Auth/Auth";
